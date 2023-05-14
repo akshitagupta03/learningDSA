f58126df68c51247aa3579700590e7bb1fc60803
Akshita Gupta <akshita.g1507@gmail.com>
2023-05-14 19:25:04 +0530

    code for maximum ice cream bars

---
+++ leetcode_medium/leetcode1833.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count=0;
+        int sum=0;
+        
+        for(int i=0; i<costs.size(); i++){
+            sum += costs[i];
+            if(sum <= coins)
+                count++;
+            else
+                break;
+        }
+        return count;
+    }
+};
