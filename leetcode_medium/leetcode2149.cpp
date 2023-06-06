cfa1c3b40f284b48c46549d8136f31345924481c
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-06 16:51:33 +0530

    rearrange elements by sign

---
+++ leetcode_medium/leetcode2149.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> v1, v2, v3;
+        
+        for(auto i:nums){
+            if(i<0) v1.push_back(i);
+            else v2.push_back(i);
+        }
+        for(int i=0;i<v1.size();i++){
+            v3.push_back(v2[i]);
+            v3.push_back(v1[i]);
+        }
+        return v3;
+    }
+};
