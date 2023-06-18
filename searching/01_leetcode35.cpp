e3456a4fde37794738bafd4c3cb017c9ab8b938b
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-18 23:15:48 +0530

    added two questions from binary search

---
+++ searching/01_leetcode35.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size()-1;
+
+    while(low < high){
+        int mid = (low+high)/2;
+        if(target == nums[mid]){
+            return mid;
+        }
+        else if(target < nums[mid]){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    
+    if(target <= nums[low]){
+        return low;
+    }
+    else{
+        return low+1;
+    }
+}
+
+int main()
+{
+
+        
+    return 0;
+}
