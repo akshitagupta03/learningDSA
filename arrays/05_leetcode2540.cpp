0d5a7c245585fe95dd302986145f962b12a24961
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-20 23:12:02 +0530

    two more questions from leetcode

---
+++ arrays/05_leetcode2540.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getCommon(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> m;
+
+    for(int i=0; i<nums1.size(); i++){
+        m[nums1[i]]++;
+    }
+
+    for(auto n : nums2){
+        if(m[n] > 0){
+            return n;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+        
+    return 0;
+}
