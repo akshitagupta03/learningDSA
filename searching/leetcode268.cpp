78bd4ca1582b41e43f9a8ee97d25871e8f59445e
Akshita Gupta <akshita.g1507@gmail.com>
2023-06-28 23:31:16 +0530

    two questions from searching

--- searching/leetcode268.cpp
+++ searching/leetcode268.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int missingNumber(vector<int>& nums) {
+int missingNumber(vector<int>& nums) 
+{
     int n = nums.size();
     int sum = (n*(n+1))/2;
     int actualSum = 0;
