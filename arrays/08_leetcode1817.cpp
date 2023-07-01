17f7fb21702671ab1c317deb245090ce0f7400f4
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-01 17:24:47 +0530

    added three array questions

---
+++ arrays/08_leetcode1817.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        unordered_map<int, unordered_set<int>> m;
+        vector<int> ans(k, 0);
+
+        for(int i=0; i<logs.size(); i++){
+            m[logs[i][0]].insert(logs[i][1]);
+        }
+
+        for(auto i : m){
+            ans[i.second.size()-1]++;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+        
+    return 0;
+}
