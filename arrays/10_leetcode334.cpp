4c11e4368f6ff591d8950655b14331dd2a64485b
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-02 23:51:58 +0530

    some more questions from leetcode

---
+++ arrays/10_leetcode334.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int a = INT_MAX;
+        int b = INT_MAX;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] <= a)
+                a = nums[i];
+
+            else if(nums[i] <= b)
+                b = nums[i];
+
+            else
+                return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+
+        
+    return 0;
+}
