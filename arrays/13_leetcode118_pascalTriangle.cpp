068d391a291bb8b870fcea435f43a849498e853f
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-02 23:53:25 +0530

    code for pascal's triangle

---
+++ arrays/13_leetcode118_pascalTriangle.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for(int i=0; i<numRows; i++){
+            vector<int> temp(i+1);
+            temp[0] = 1;
+            for(int j=1; j<i; j++){
+                temp[j] = ans[i-1][j-1] + ans[i-1][j];
+            }
+            if(i != 0){
+                temp[i] = 1;
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+        
+    return 0;
+}
