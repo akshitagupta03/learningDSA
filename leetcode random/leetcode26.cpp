2376725a93d8adba889aa5f242d7a6f893a090bd
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-10 22:47:51 +0530

    remove duplicates from sorted array

---
+++ leetcode random/leetcode26.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> s;
+        for(int i=0; i<nums.size(); i++){
+            s.insert(nums[i]);
+        }
+
+        int j = 0;
+        for(auto i:s){
+            nums[j] = i;
+            j++;
+        }
+
+        return s.size();
+    }
+};
+
+int main()
+{
+
+        
+    return 0;
+}
