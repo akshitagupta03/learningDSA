8b3a64c78e2b3a8f10d32a86a692bd854406d612
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-20 23:19:49 +0530

    added questions from starters 99

---
+++ codechef/starters 99/q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	
+	if(n==6 || n==7 || n==8){
+	    cout << "YES" << endl;
+	}
+	else{
+	    cout << "NO" << endl;
+	}
+	return 0;
+}
