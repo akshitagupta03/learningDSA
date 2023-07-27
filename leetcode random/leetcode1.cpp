6cb5715055ad26ecf3f964ca7c9a32b2fe766ef4
Akshita Gupta <akshita.g1507@gmail.com>
2023-07-27 23:27:14 +0530

    two sum

---
+++ leetcode random/leetcode1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m.find(target - nums[i]) != m.end()){
+                return {m[target-nums[i]], i};
+            }
+            else{
+                m[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
+
+int main()
+{
+
+        
+    return 0;
+}
