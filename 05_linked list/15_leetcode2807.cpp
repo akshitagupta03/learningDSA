a952686bcf883a05080a92046223273fcf52181e
Akshita Gupta <akshita.g1507@gmail.com>
2023-08-27 12:47:25 +0530

    insert gcd in linked list

---
+++ 05_linked list/15_leetcode2807.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int gcd(int a, int b)
+    {
+        // Find Minimum of a and b
+        int result = min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+    
+        return result;
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* temp = head->next;
+        ListNode* prev = head;
+        while(temp != NULL){
+            int a = gcd(prev->val, temp->val);
+            ListNode* curr = new ListNode(a);
+            prev->next = curr;
+            curr->next = temp;
+            prev = temp;
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
