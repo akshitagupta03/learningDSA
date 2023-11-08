49e9e5a03f0cf2a2c4077d30956a0be6d4d7ff1b
Akshita <akshita.g1507@gmail.com>
2023-11-08 19:35:37 +0530

    added questions for starters 103 (div-3)

---
+++ codechef/starters 106/q2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define rep(x, start, end) for(ll x = start; x < end; x++)
+#define rrep(x, start, end) for(ll x = start; x >= end; x--)
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+//-------------------------- Debug -------------------------/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    
+    vi a(n);
+    int count = 0;
+    
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    
+    for(int i=0; i<n-k+1; i++){
+        for(int j=i; j<i+k; j++){
+            if(a[j] % 2 == 1){
+                count++;
+                break;
+            }
+        }
+    }
+    
+    p(count);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
