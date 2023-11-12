8d8e489c7065f7b78ba44ab213581f6221d5e0fa
Akshita Gupta <akshita.g1507@gmail.com>
2023-11-12 14:46:01 +0530

    valid parentheses

---
+++ 12_stack/03_leetcode20.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> temp;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(temp.empty()){
+                temp.push(s[i]);
+            }
+            else if((temp.top() == '(' && s[i] == ')') || (temp.top() == '{' && s[i] == '}') || (temp.top() == '[' && s[i] == ']'))
+            {
+                temp.pop();
+            }
+            else{
+                temp.push(s[i]);
+            }
+        }
+
+        if(temp.empty()){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+
+    
+    return 0;
+}
