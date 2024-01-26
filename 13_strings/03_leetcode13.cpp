96cb174188260eb0268627a81fac00ff860904a7
Akshita <akshita.g1507@gmail.com>
2024-01-26 16:02:23 +0530

    roman to integer

---
+++ 13_strings/03_leetcode13.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        unordered_map<char, int> m;
+
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(m[s[i+1]] > m[s[i]])
+            {
+                ans -= m[s[i]];
+            }
+            else
+            {
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+    
+    return 0;
+}
