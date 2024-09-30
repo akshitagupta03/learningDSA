738bc1728e29f759a41928efa95afcedae3b11d7
Akshita <akshita.g1507@gmail.com>
2024-10-01 00:49:33 +0530

    kth largest sum subarray

---
+++ 17_heap/05_kthLargestSumSubarray.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            int sum = 0;
+            for(int j=i; j<arr.size(); j++)
+            {
+                sum += arr[j];
+                if(pq.size() < k)
+                {
+                    pq.push(sum);
+                }
+                else
+                {
+                    if(sum > pq.top())
+                    {
+                        pq.pop();
+                        pq.push(sum);
+                    }
+                }
+            }
+        }
+        
+        return pq.top();
+    }
+};
+
+int main(){
+
+    
+    return 0;
+}
