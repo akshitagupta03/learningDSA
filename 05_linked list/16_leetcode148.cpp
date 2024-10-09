896a1ba260a8b8e8f2c557447dc5e53af26630ed
Akshita <akshita.g1507@gmail.com>
2024-10-09 18:25:03 +0530

    sort linked list

--- 05_linked list/16_leetcode148.cpp
+++ 05_linked list/16_leetcode148.cpp
@@ -11,36 +11,82 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
+    ListNode* getMid(ListNode* head) 
+    {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
 
-        if(temp == NULL)
+        while(fast && fast->next) 
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right)
+    {
+        if(left == NULL)
+        {
+            return right;
+        }
+        if(right == NULL)
+        {
+            return left;
+        }
+
+        ListNode* temp = new ListNode(0);
+        ListNode* curr = temp;
+
+        while(left && right)
+        {
+            if(left->val <= right->val)
+            {
+                curr->next = left;
+                left = left->next;
+            }
+            else
+            {
+                curr->next = right;
+                right = right->next;
+            }
+            curr = curr->next;
+        }
+
+        while(left)
+        {
+            curr->next = left;
+            left = left->next;
+            curr = curr->next;
+        }
+        while(right)
+        {
+            curr->next = right;
+            right = right->next;
+            curr = curr->next;
+        }
+
+        return temp->next;
+    }
+
+    ListNode* sortList(ListNode* head) 
+    {
+        if(head == NULL || head->next == NULL)
         {
             return head;
         }
 
-        while(temp)
-        {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
-        sort(arr.begin(), arr.end(), greater<int>());
+        ListNode* mid = getMid(head);
 
-        temp = head;
-        while(temp != NULL)
-        {
-            temp->val = arr.back();
-            arr.pop_back();
-            temp = temp->next;
-        }
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
 
-        return head;
+        left = sortList(left);
+        right = sortList(right);
+
+        ListNode* result = merge(left, right);
+
+        return result;
     }
 };
-
-int main(){
-
-    
-    return 0;
-}
