a9c010442ec7899eb8db7b0350f0f6aecd7f2179
Akshita <akshita.g1507@gmail.com>
2024-10-29 00:01:45 +0530

    minimum path sum

---
+++ 14_dynamicProgramming/10_leetcode64.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // space optimisation
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prev(n, 0);
+
+        for(int i=0; i<m; i++)
+        {
+            vector<int> temp(n, 0);
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0) 
+                {
+                    temp[j] = grid[0][0];
+                }
+                else 
+                {
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i > 0) up = grid[i][j] + prev[j];
+                    if(j > 0) left = grid[i][j] + temp[j-1];
+
+                    temp[j] = min(up, left);
+                }
+            }
+            prev = temp;
+        }
+
+        return prev[n-1];
+    }
+};
+
+int main(){
+
+    
+    return 0;
+}
