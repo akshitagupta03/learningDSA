7b88b2472bd6945bad5eca7a874f79f02235dfe4
Akshita <akshita.g1507@gmail.com>
2024-11-07 02:01:00 +0530

    longest increasing subsequence

---
+++ 14_dynamicProgramming/29_leetcode300.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int ind=n-1; ind>=0; ind--)
+        {
+            for(int prev=ind-1; prev >= -1; prev--)
+            {
+                int notPick = dp[ind+1][prev+1];
+                int pick = 0;
+
+                if(prev == -1 || nums[ind] > nums[prev])
+                {
+                    pick = 1 + dp[ind+1][ind+1];
+                }
+
+                dp[ind][prev+1] = max(pick, notPick);
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+int main(){
+
+    
+    return 0;
+}
